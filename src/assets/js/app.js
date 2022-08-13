876c6414aa1920b0389503757e37e1a1fce6e803
Ali.t.nazari <91375198+Silent-Watcher@users.noreply.github.com>
2022-08-13 16:11:14 +0430

    finish qr code section

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,7 @@
 let $ = document;
 const urlWrapper = $.querySelector('#input_section');
 const urlInput = urlWrapper.lastElementChild;
+const generateBtn = $.querySelector('#generate_button');
 // input label effect
 urlInput.addEventListener('focus', function () {
   this.parentElement.classList.add('filled');
@@ -12,12 +13,23 @@ urlInput.addEventListener('blur', function () {
   else this.previousElementSibling.classList.remove('active');
 });
 //
-let qrcode = new QRCode($.getElementById("qr_code"), {
-	text: "https://github.com/Silent-Watcher",
-	width: 250,
-	height: 250,
-	colorDark : "#303030",
-	colorLight : "#f5f5f5",
-	correctLevel : QRCode.CorrectLevel.H
+urlInput.addEventListener('input', function () {
+  if (this.value.trim().length > 0) generateBtn.classList.remove('disabled');
+  else generateBtn.classList.add('disabled');
+});
+let qrcode = new QRCode($.getElementById('qr_code'), {
+  text: 'https://github.com/Silent-Watcher',
+  width: 250,
+  height: 250,
+  colorDark: '#303030',
+  colorLight: '#f5f5f5',
+  correctLevel: QRCode.CorrectLevel.H,
 });
 
+generateBtn.addEventListener('click', () => {
+  localStorage.setItem('url', urlInput.value);
+  qrcode.makeCode(localStorage.getItem('url')); 
+  generateBtn.classList.add('disabled');
+  urlInput.value = '';
+  urlInput.previousElementSibling.classList.remove('active');
+});
